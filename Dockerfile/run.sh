de9cbca48a6c8c25fb929236c4439a014fae3cc4
José Villagrán <josevillagran18@gmail.com>
2020-07-01 00:33:44 -0400

    Add complete LaSOT loader and modified run.sh

--- Dockerfile/run.sh
+++ Dockerfile/run.sh
@@ -4,4 +4,6 @@ GPU=$1
 shift
 DIR=/project 
 
-NV_GPU="$GPU" nvidia-docker run --rm --name repmet_training -v `pwd`/:$DIR:rw -t repmet $@
+echo GPU = $GPU
+
+NV_GPU="$GPU" nvidia-docker run --rm --name repmet_training -v $HOME/:$DIR:rw -t repmet $@
--- data_loading/sets/LaSOT.py
+++ data_loading/sets/LaSOT.py
@@ -16,11 +16,13 @@ from __future__ import print_function
 from PIL import Image, ImageFile
 from os.path import join
 import os
+from os import listdir
 import numpy as np
 import scipy.io
 import tarfile
+import zipfile
 import shutil
-
+import csv
 from torch.utils.data import DataLoader
 from torch.utils.data.dataset import Dataset
 from torchvision.datasets.utils import download_url
@@ -30,17 +32,22 @@ import sys
 
 from utils.debug import set_working_dir
 from config.config import config
+from utils.download import DownloadGDrive
 import matplotlib.pyplot as plt
 
 class LaSOTDataset(Dataset):
     # setup some class paths
     sub_root_dir = 'LaSOT'
     download_url_prefix = 'https://cis.temple.edu/lasot/data/category/'
+    id_drive = '1v09JELSXM_v7u3dF7akuqqkVG8T1EK2_'
     images_dir = 'Images'
 
     def __init__(self,
                  root_dir,
+                 target_obj,
                  split='train',
+                 crop=True,
+                 drive=False,
                  transform=None,
                  target_transform=None,
                  force_download=False,
@@ -59,29 +66,35 @@ class LaSOTDataset(Dataset):
         # set instance variables
         self.root_dir = join(os.path.expanduser(root_dir), self.sub_root_dir)
         self.split = split
+        self.crop = crop
         self.transform = transform
         self.target_transform = target_transform
         self.labels = []
 
         # check if data exists, if not download
-        self.download(force=force_download)
+        self.download(target_obj, force=force_download)
 
         # load the data samples for this split
-        self.data, self.labels, self.categories = self.load_data_split(categories_subset=categories_subset)
-        self.samples = list(zip(self.data, self.labels))
+        # categories(e.x bicycle) and labels (ex. bicycle_1, bicycle_2 ...)
+        self.data, self.labels, self.categories, self.gt = self.load_data_split(categories_subset=categories_subset)
+        self.samples = list(zip(self.data, self.categories, self.labels, self.gt))
 
-        self.n_categories = len(np.unique(self.labels))
+        self.n_videos = len(np.unique(self.labels))
 
     def __len__(self):
         return len(self.samples)
 
     def __getitem__(self, index):
         # get the data sample
-        sample_data, sample_target = self.samples[index]
+        sample_data, sample_target, sample_categorie, sample_gt = self.samples[index]
+
+        box = (sample_gt[0], sample_gt[1], sample_gt[0]+sample_gt[2], sample_gt[1]+sample_gt[3])
 
         # load the image
-        x = self.load_img(join(join(self.root_dir, self.images_dir), "%s" % sample_data))
-        y = sample_target
+        x = self.load_img(join(join(self.root_dir, sample_categorie, sample_target, "img"), "%s" % sample_data),
+                          crop=self.crop, tuple_crop=box)
+        
+        y = sample_categorie # Cuidado. Lo más posible es que deba modificar
 
         # perform the transforms
         if self.transform:
@@ -92,7 +105,7 @@ class LaSOTDataset(Dataset):
 
         return x, y
 
-    def download(self, target_obj, force=False):
+    def download(self, target_obj, force=False, drive=False):
         """
         
 
@@ -108,97 +121,84 @@ class LaSOTDataset(Dataset):
 
         """
         # check for existence, if so return
-        if os.path.exists(join(self.root_dir, 'Images')) and os.path.exists(join(self.root_dir, 'Annotation')):
-            if not force and len(os.listdir(join(self.root_dir, 'Images'))) == 120:
-                print('Files already downloaded and verified')
-                return
+        for obj in target_obj:
+            if os.path.exists(join(self.root_dir, obj)):
+                if not force:
+                    print('File {} already downloaded and verified'.format(obj))
+                    return
+                else:
+                    shutil.rmtree(join(self.root_dir, obj))
+    
+            # make the dirs and start the downloads
+            os.makedirs(self.root_dir, exist_ok=True)
+            filename = obj
+            zip_filename = filename + '.zip'
+            url = join(self.download_url_prefix, zip_filename)
+            
+            if drive:
+                DownloadGDrive(self.id_drive, self.root_dir)
             else:
-                shutil.rmtree(self.root_dir)
+                download_url(url, self.root_dir, zip_filename, None)
+            
+            zipfile.ZipFile.extractall(join(self.root_dir, zip_filename))
+            os.remove(join(self.root_dir, zip_filename))
 
-        # make the dirs and start the downloads
-        os.makedirs(self.root_dir, exist_ok=True)
-        filename = 'images'
-        tar_filename = filename + '.tar'
-        url = join(self.download_url_prefix, tar_filename)
-        download_url(url, self.root_dir, tar_filename, None)
-        with tarfile.open(join(self.root_dir, tar_filename), 'r') as tar_file:
-            tar_file.extractall(self.root_dir)
-        os.remove(join(self.root_dir, tar_filename))
+    def load_data_split(self, subcategories=None):
 
-        filename = 'annotation'
-        tar_filename = filename + '.tar'
-        url = join(self.download_url_prefix, tar_filename)
-        download_url(url, self.root_dir, tar_filename, None)
-        with tarfile.open(join(self.root_dir, tar_filename), 'r') as tar_file:
-            tar_file.extractall(self.root_dir)
-        os.remove(join(self.root_dir, tar_filename))
-
-        filename = 'lists'
-        tar_filename = filename + '.tar'
-        url = join(self.download_url_prefix, tar_filename)
-        download_url(url, self.root_dir, tar_filename, None)
-        with tarfile.open(join(self.root_dir, tar_filename), 'r') as tar_file:
-            tar_file.extractall(self.root_dir)
-        os.remove(join(self.root_dir, tar_filename))
-
-    def load_data_split(self, categories_subset=None):
-
-        # assert we can do this split
         assert self.split in ['train', 'test', 'rand', 'val']
 
         # load the samples and their labels
         data = []
         categories = []
-        if self.split == 'rand':
-            all_samples = scipy.io.loadmat(join(self.root_dir, 'file_list.mat'))
-            #todo will need to add some rand split settings
-        elif self.split == 'train':
-            all_samples = scipy.io.loadmat(join(self.root_dir, 'train_list.mat'))
-        elif self.split == 'test' or self.split == 'val':
-            all_samples = scipy.io.loadmat(join(self.root_dir, 'test_list.mat'))
-
-        all_data = all_samples['file_list']
-        all_labels = all_samples['labels']
-
-        for i in range(len(all_labels)):
-            category = int(all_labels[i])
-            if categories_subset:
-                if category in categories_subset:
-                    data.append(all_data[i][0][0])
-                    categories.append(category)
-            else:  # categories_subset is None so add all
-                data.append(all_data[i][0][0])
-                categories.append(category)
-
-        # Build categories to labels (cats can be names, labels are ints starting from 0)
-        self.categories_to_labels = {}
-        self.labels_to_categories = {}
-        for c in categories:
-            if c not in self.categories_to_labels:
-                self.categories_to_labels[c] = len(self.categories_to_labels)
-                self.labels_to_categories[self.categories_to_labels[c]] = c
-
-        # Build labels list corresponding to each sample
         labels = []
-        for c in categories:
-            labels.append(self.categories_to_labels[c])
+        gt = []
+        path = join(self.root_dir)
+        for f_obj in listdir(path):
+            if subcategories:
+                categories.append(f_obj)
+                if f_obj in subcategories:
+                    for f in listdir(join(path, f_obj)):
+                        labels.append(f)
+                        # Read GT
+                        f = open(os.path.join(path, f_obj, f,'groundtruth.txt'), 'r')
+                        data = f.readlines()
+                        reader = csv.reader(data)
+                        for idx, row in enumerate(reader):
+                            gt.append(row)
+                        # Read Image
+                        for img in listdir(join(path, f_obj, f, "img")): 
+                            data.append(join(path, f_obj, f, "img", img))
+                        
+            else:
+                categories.append(f_obj)
+                for f in listdir(join(path, f_obj)):
+                    labels.append(f)
+                    
+                    # Read GT
+                    f = open(os.path.join(path, f_obj, f,'groundtruth.txt'), 'r')
+                    data = f.readlines()
+                    reader = csv.reader(data)
+                    for idx, row in enumerate(reader):
+                        gt.append(row)
+                    
+                    for img in listdir(join(path, f_obj, f, "img")):
+                        data.append(join(path, f_obj, f, "img", img))
 
-        # set the data, categories and labels used in this dataset
-        # (initially ordered with self.samples and not unique, careful with access post shuffling)
-        self.categories = categories
-        self.labels = labels
-        self.data = data
-
-        return data, labels, categories
+        return data, labels, categories, gt
 
     @staticmethod
-    def load_img(path):
+    def load_img(path, crop=False, tuple_crop=None, rot=False, angle=0):
 
         # todo either turn image to tensor in transform or do here
         # Load the image
         ImageFile.LOAD_TRUNCATED_IMAGES = True
         image = Image.open(path).convert('RGB')
-
+        
+        if crop:
+            image = image.crop(tuple_crop)
+        
+        if rot:
+            image = image.rotate(angle) # angle in degree
 
 
         return image
@@ -225,39 +225,30 @@ class LaSOTDataset(Dataset):
 
 if __name__ == "__main__":
     
-    
-    for obj in open(join(config.dataset.root_dir,'Objetos.txt'), "r"):
-        print(obj)
-    
-    
-    
-    
-    
-    # use this for debugging and checks
 
-    # set the working directory as appropriate
-    # set_working_dir()
-
-    # # load the dataset
-    # dataset = LaSOTDataset(root_dir=config.dataset.root_dir, split='test')
-
-    # # print the stats
-    # print(dataset.stats())
-
-    # # lets plot some samples
-    # fig = plt.figure()
+    set_working_dir()
     
-    # ex = dataset.__getitem__(0)
+    target_obj = ["coin"]
+    # load the dataset
+    dataset = LaSOTDataset(root_dir=config.dataset.root_dir, target_obj=target_obj, drive=True, split='test')
+
+    # print the stats
+    print(dataset.stats())
+
+    # lets plot some samples
+    fig = plt.figure()
     
-    # for i in range(len(dataset)):
-    #     sample = dataset[i]
+    ex = dataset.__getitem__(0)
+    
+    for i in range(len(dataset)):
+        sample = dataset[i]
 
-    #     ax = plt.subplot(1, 4, i + 1)
-    #     plt.tight_layout()
-    #     ax.set_title('Sample %d - Class %s' % (i, dataset.labels_to_categories[sample[1]]))  # convert label to categ.
-    #     ax.axis('off')
-    #     plt.imshow(sample[0])  # todo when tensor will need to convert tensor to img
+        ax = plt.subplot(1, 4, i + 1)
+        plt.tight_layout()
+        ax.set_title('Sample %d - Class %s' % (i, sample[1]))  # convert label to categ.
+        ax.axis('off')
+        plt.imshow(sample[0])  # todo when tensor will need to convert tensor to img
 
-    #     if i == 3:
-    #         plt.show()
-    #         break
+        if i == 3:
+            plt.show()
+            break
