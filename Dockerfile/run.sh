7a3f12cd8d29863dfb7d8c1bc565519e0f99e2bd
JoseVillagranE <josevillagran18@gmail.com>
2020-08-24 03:39:55 -0400

    Add change to Dockerfile folder

--- Dockerfile/run.sh
+++ Dockerfile/run.sh
@@ -2,9 +2,9 @@
 
 GPU=$1 
 shift
-DIR=/project/RepMet/models/resnet/03 
+DIR=/Repmet/models 
 
 echo GPU = $GPU
 echo Container Directory: $DIR
 
-NV_GPU="$GPU" nvidia-docker run --rm --name repmet_training -v $HOME/:$DIR:rw -t repmet $@
+NV_GPU="$GPU" nvidia-docker run --rm --name repmet_training -v $HOME/:$DIR -t repmet $@
