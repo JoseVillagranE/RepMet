2dfbdfaf53a8f7e381374d26197be257f5ca3919
José <>
2020-12-13 17:07:13 -0300

    Replace norm for mse in angle loss

--- losses/angle_loss.py
+++ losses/angle_loss.py
@@ -9,8 +9,7 @@ class AngleLoss(nn.Module):
         super().__init__()
         # self.alpha_1 = config.train.alpha_1
         # self.alpha_2 = config.train.alpha_2
-        # self.mse = nn.MSELoss(reduction='none')
-
+        self.mse = nn.MSELoss(reduction='none')
         self.cross_entropy = nn.CrossEntropyLoss()
 
 
@@ -19,6 +18,5 @@ class AngleLoss(nn.Module):
         # loss = self.alpha_1*self.mse(sin_pred, sin_label) + \
         #        self.alpha_1*self.mse(cos_pred, cos_label) + \
         #        self.alpha_2*(sin_pred.pow(2) + cos_pred.pow(2) - 1)
-
-        loss = self.cross_entropy(cell_pred, k) + torch.norm(angle - theta_pred)
+        loss = self.cross_entropy(cell_pred, k) + self.mse(theta_pred, angle)
         return loss.mean()
